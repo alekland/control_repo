ec143c45878a8e5cccd5b7465a7b009dd91c1cf1
Alek <alekland@users.noreply.github.com>
2018-12-21 20:57:16 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragents
-  dockeragent:node {'web.puppet.vm':}
-  dockeragent:node {'db.puppet.vm':}
+  dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'db.puppet.vm':}
 }
